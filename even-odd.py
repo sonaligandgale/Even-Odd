ce539950591ff58866ec326f2bff40a6380a4a2f
sonaligandgale <sonaligandgalekar@gmail.com>
2025-10-23 15:20:00 +0530

    Create even-odd.py

---
+++ even-odd.py
@@ -0,0 +1,14 @@
+# Even and Odd Counter Program
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+even_count = 0
+odd_count = 0
+
+for num in numbers:
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+print("Even numbers:", even_count)
+print("Odd numbers:", odd_count)
